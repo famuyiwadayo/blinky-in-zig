9a394f6c6440560e05f322ed27d9871c6147b891
famuyiwadayo <famuyiwadayodaniel@gmail.com>
2023-03-22 08:55:17 +0100

    initial commit

---
+++ build.zig
@@ -0,0 +1,87 @@
+const std = @import("std");
+const rp2040 = @import("libs/rp2040/build.zig");
+const microzig = @import("libs/microzig/build.zig");
+const uf2 = @import("libs/uf2/src/main.zig");
+
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    // const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = microzig.addEmbeddedExecutable(b, .{
+        .name = "blink",
+        .root_source_file = .{
+            .path = "src/main.zig",
+        },
+        .backing = .{
+            .board = rp2040.boards.raspberry_pi_pico,
+
+            // instead of a board, you can use the raw chip as well
+            // .chip = atmega.chips.atmega328p,
+        },
+        .optimize = optimize,
+    });
+
+    const uf2_step = uf2.Uf2Step.create(b, exe.inner, .{ .family_id = .RP2040 });
+    uf2_step.install();
+
+    // const exe = b.addExecutable(.{
+    //     .name = "zig",
+    //     // In this case the main source file is merely a path, however, in more
+    //     // complicated build scripts, this could be a generated file.
+    //     .root_source_file = .{ .path = "src/main.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    exe.install();
+
+    // This *creates* a RunStep in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    // const run_cmd = exe.run();
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    // run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    // if (b.args) |args| {
+    //     run_cmd.addArgs(args);
+    // }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    // const run_step = b.step("run", "Run the app");
+    // run_step.dependOn(&run_cmd.step);
+
+    // Creates a step for unit testing.
+    // const exe_tests = b.addTest(.{
+    //     .root_source_file = .{ .path = "src/main.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    // const test_step = b.step("test", "Run unit tests");
+    // test_step.dependOn(&exe_tests.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const microzig = @import("microzig");
+const rp2040 = microzig.hal;
+const time = rp2040.time;
+
+const pins_config = rp2040.pins.GlobalConfiguration{ .GPIO7 = .{
+    .name = "led",
+    .direction = .out,
+} };
+
+pub fn main() !void {
+    const pins = pins_config.apply();
+
+    while (true) {
+        pins.led.toggle();
+        time.sleep_ms(500);
+    }
+}
